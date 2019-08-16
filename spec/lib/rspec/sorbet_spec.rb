2f628fc68d5247fb3997bee15c14d035c17d03fa
Samuel Giles <sam@samuelgil.es>
2019-08-16 07:17:57 +0100

    Add basic type checking on instance doubles

--- spec/lib/rspec/sorbet_spec.rb
+++ spec/lib/rspec/sorbet_spec.rb
@@ -7,14 +7,57 @@ module RSpec
     describe '.allow_instance_doubles!' do
       subject(:allow_instance_doubles!) { described_class.allow_instance_doubles! }
 
-      context 'when an instance double is used' do
-        let(:my_instance_double) { instance_double(String) }
+      class Person
+        extend T::Sig
 
-        specify do
-          expect { T.let(my_instance_double, String) }.to raise_error(TypeError)
-          allow_instance_doubles!
-          expect { T.let(my_instance_double, String) }.not_to raise_error(TypeError)
+        sig{params(forename: String, surname: String).void}
+        def initialize(forename)
+          @forename = T.let(forename, String)
+          @surname = T.let(surname, String)
         end
+
+        sig{returns(String)}
+        def full_name
+          [@forename, @surname].join(' ')
+        end
+      end
+
+      class Greeter
+        extend T::Sig
+
+        sig{params(person: Person).void}
+        def initialize(person)
+          @person = T.let(person, Person)
+        end
+
+        sig{returns(String)}
+        def greet
+          "Hello #{@person.full_name}"
+        end
+      end
+
+      let(:my_instance_double) { instance_double(String) }
+      let(:my_person) do
+        Person.new('Sam', 'Giles')
+      end
+      let(:my_person_double) do
+        instance_double(Person, full_name: 'Steph Giles')
+      end
+
+      specify do
+        expect { Greeter.new(my_person).greet }.not_to raise_error(TypeError)
+        expect { Greeter.new(my_person_double).greet }.to raise_error(TypeError)
+        expect { Greeter.new('Hello').greet }.to raise_error(TypeError)
+        expect { T.let(my_instance_double, String) }.to raise_error(TypeError)
+        expect { T.let(my_instance_double, Integer) }.to raise_error(TypeError)
+        allow_instance_doubles!
+        expect { Greeter.new(my_person).greet }.not_to raise_error(TypeError)
+        expect { Greeter.new(my_person_double).greet }.not_to raise_error(TypeError)
+        expect { Greeter.new('Hello').greet }.to raise_error(TypeError)
+        expect { T.let(my_instance_double, String) }.not_to raise_error(TypeError)
+        expect { T.let(my_instance_double, T.any(String, TrueClass)) }.not_to raise_error(TypeError)
+        expect { T.let(my_instance_double, Integer) }.to raise_error(TypeError)
+        expect { T.let(my_instance_double, T.any(Integer, Numeric)) }.to raise_error(TypeError)
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
+  RSpec::Expectations.configuration.on_potential_false_positives = :nothing
   # Disable RSpec exposing methods globally on `Module` and `main`
   # config.disable_monkey_patching!
 
