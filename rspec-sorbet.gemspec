f54289c2e6f7e16d0c79cc62cf8e7da29a1a60ed
Christina Chan <6462356+cchawn@users.noreply.github.com>
2019-11-05 15:39:46 -0500

    Allow all kinds of doubles (Instance, Class, Object) (#1)
    * Fix the gemspec
    * Add ability to allow any kind of double (Class, Instance, Object)
      * rename RSpec::Sorbet::InstanceDoubles to RSpec::Sorbet::Doubles
      * keep `allow_instance_doubles!` method for backwards compatibility
      * introduce new `allow_doubles!` method
    * Bump version

--- rspec-sorbet.gemspec
+++ rspec-sorbet.gemspec
@@ -1,7 +1,6 @@
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-require 'rspec/sorbet'
 require 'rspec/sorbet/version'
 
 Gem::Specification.new do |spec|
--- spec/lib/rspec/sorbet_spec.rb
+++ spec/lib/rspec/sorbet_spec.rb
@@ -4,9 +4,7 @@ require 'rspec/sorbet'
 
 module RSpec
   describe Sorbet do
-    describe '.allow_instance_doubles!' do
-      subject(:allow_instance_doubles!) { described_class.allow_instance_doubles! }
-
+    shared_context 'instance double' do
       class Person
         extend T::Sig
 
@@ -51,7 +49,9 @@ module RSpec
       let(:another_person) do
         instance_double(Person, full_name: 'Yasmin Collins')
       end
+    end
 
+    shared_examples 'it allows an instance double' do
       specify do
         expect { Greeter.new(my_person).greet }.not_to raise_error(TypeError)
         expect { Greeter.new(my_person_double).greet }.to raise_error(TypeError)
@@ -60,7 +60,7 @@ module RSpec
         expect { T.let(my_instance_double, Integer) }.to raise_error(TypeError)
         expect { Greeter.new(my_person_double).greet_others([my_person_double, another_person]) }
           .to raise_error(TypeError)
-        allow_instance_doubles!
+        subject
         expect { Greeter.new(my_person).greet }.not_to raise_error(TypeError)
         expect { Greeter.new(my_person_double).greet }.not_to raise_error(TypeError)
         expect { Greeter.new('Hello').greet }.to raise_error(TypeError)
@@ -70,6 +70,47 @@ module RSpec
         expect { T.let(my_instance_double, T.any(String, TrueClass)) }.not_to raise_error(TypeError)
         expect { T.let(my_instance_double, Integer) }.to raise_error(TypeError)
         expect { T.let(my_instance_double, T.any(Integer, Numeric)) }.to raise_error(TypeError)
+      end
+    end
+
+    after do
+      T::Configuration.inline_type_error_handler = nil
+      T::Configuration.call_validation_error_handler = nil
+    end
+
+    describe '.allow_instance_doubles!' do
+      subject { described_class.allow_instance_doubles! }
+
+      include_context 'instance double'
+      it_should_behave_like 'it allows an instance double'
+    end
+
+    describe '.allow_doubles!' do
+      subject { described_class.allow_doubles! }
+
+      describe 'instance doubles' do
+        include_context 'instance double'
+        it_should_behave_like 'it allows an instance double'
+      end
+
+      describe 'class doubles' do
+        let(:my_class_double) { class_double(String) }
+
+        specify do
+          expect { T.let(my_class_double, String) }.to raise_error(TypeError)
+          subject
+          expect { T.let(my_class_double, String) }.not_to raise_error(TypeError)
+        end
+      end
+
+      describe 'object doubles' do
+        let(:my_object_double) { object_double(String) }
+
+        specify do
+          expect { T.let(my_object_double, String) }.to raise_error(TypeError)
+          subject
+          expect { T.let(my_object_double, String) }.not_to raise_error(TypeError)
+        end
       end
     end
   end
